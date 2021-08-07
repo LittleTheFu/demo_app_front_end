b1d84612ff0bccf7aa81904e31a7c678902425ff
LittleTheFu <lttlstpd@live.com>
2021-08-07 17:59:35 +0800

    add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,19 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Login from './login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeBar from './HomeBar';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
+export default function App() {
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <HomeBar />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
