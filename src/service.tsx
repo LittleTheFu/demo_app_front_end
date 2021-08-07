4b2eb1b019e75ada6a7bd8f1c04c0b47198fdc72
LittleTheFu <lttlstpd@live.com>
2021-08-07 18:34:57 +0800

    add network interface

---
+++ src/service.tsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+function rawObjectPost<T>(
+    url: string,
+    data: object,
+    resolve: (data: T) => void,
+    headerContent: object = {},
+    reject?: (error: Error) => void,
+): Promise<T> {
+    return axios
+        .post(url, JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json',
+                ...headerContent,
+            },
+        })
+        .then(response => {
+            const { data } = response;
+            // console.log(data);
+            resolve(data);
+        })
+        .catch(err => {
+            if (err.response) {
+                // console.log('err.response');
+                // console.log(err.response.data);
+                // console.log(err.response.status);
+                // console.log(err.response.headers);
+
+                // if ('error' in err.response.data) {
+                //     if (reject) {
+                //         reject(new Error(err.response.data.error));
+                //     } else {
+                //         store.dispatch({ type: OPEN_HINT, payload: { hintMsg: err.response.data.error } });
+                //     }
+                // }
+            } else if (err.request) {
+                // console.log('err.request');
+                // console.log(err.request);
+            } else {
+                // console.log('Error', err.message);
+            }
+            // console.log(err.config);
+
+            return err;
+        });
+}
+
+class AccessData {
+    // accessToken: string;
+}
+
+const loginUrl = 'login';
+export const postLogin = (
+    username: string,
+    password: string,
+    resolve: (data: AccessData) => void,
+    reject?: (data: Error) => void,
+): Promise<AccessData> => {
+    return rawObjectPost(loginUrl, { username: username, password: password }, resolve, {}, reject);
+};
+
