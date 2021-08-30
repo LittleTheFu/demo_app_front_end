4d1fcd9deb716478b36cea5442b4512835fe038a
LittleTheFu <lttlstpd@live.com>
2021-08-30 10:40:58 +0800

    fix styles of title card

--- src/component/TitleCard.tsx
+++ src/component/TitleCard.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { UserHead } from './UserHead';
 
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 800,
-    },
-});
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        avatarContainer: {
+            display: 'tableCell',
+            verticalAlign: 'middle',
+        },
+        avatar: {
+            height: 40,
+            width: 40,
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+        card: {
+            padding: theme.spacing(1),
+        },
+        name: {
+            textAlign: 'center',
+        },
+        container: {
+            display: 'inline-block',
+            verticalAlign: 'middle',
+        },
+    }),
+);
 
 interface TitleCardProps {
     id: number;
@@ -20,20 +40,24 @@ interface TitleCardProps {
 
     textClick?: () => void;
     authorClick?: () => void;
-} 
+}
 
 export const TitleCard: React.FC<TitleCardProps> = (props: TitleCardProps) => {
     const classes = useStyles();
 
-    return ( 
-        <Card className={classes.root}>
+    return (
+        <Card>
             <CardActionArea onClick={props.textClick}>
                 <CardContent>
-                <UserHead size={50} userName={props.author} avatar={props.authorIcon}
-                        nameClick={props.authorClick} avatarClick={props.authorClick}></UserHead>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.title}
-                    </Typography>
+                    <div className={classes.container}>
+                        <UserHead size={50} userName={props.author} avatar={props.authorIcon}
+                            nameClick={props.authorClick} avatarClick={props.authorClick} />
+                    </div>
+                    <div className={classes.container}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {props.title}
+                        </Typography>
+                    </div>
                 </CardContent>
             </CardActionArea>
         </Card>
