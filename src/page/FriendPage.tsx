270e4121b4b8106888cae7dfde2c769983049a52
LittleTheFu <lttlstpd@live.com>
2021-09-05 11:46:53 +0800

    clean code

--- src/page/FriendPage.tsx
+++ src/page/FriendPage.tsx
@@ -1,12 +1,6 @@
-// Import React dependencies.
-import React, { useMemo, useState } from 'react'
-// Import the Slate editor factory.
+import React, { useState } from 'react'
 import { createEditor, Descendant } from 'slate'
-
-// Import the Slate components and React plugin.
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
-
-// TypeScript users only add this code
 import { BaseEditor } from 'slate'
 
 type CustomElement = { type: 'paragraph'; children: CustomText[] }
@@ -22,7 +16,8 @@ declare module 'slate' {
 
 export const FriendPage: React.FC = () => {
 
-  const editor = useMemo(() => withReact(createEditor()), [])
+  const editor = createEditor();
+  // const editor = useMemo(() => withReact(createEditor()), [])
   // Add the initial value when setting up our state.
   const [value, setValue] = useState<Descendant[]>([
     {
@@ -41,16 +36,3 @@ export const FriendPage: React.FC = () => {
     </Slate>
   )
 }
-
-
-
-// export const FriendPage: React.FC = () => {
-//     // Also you must annotate `useState<Descendant[]>` and the editor's initial value.
-//     const initialValue: CustomElement = []
-//     const [value, setValue] = useState<Descendant[]>(initialValue)
-//     return (
-//       <Slate value={value} onChange={setValue}>
-//         ...
-//       </Slate>
-//     )
-// };
