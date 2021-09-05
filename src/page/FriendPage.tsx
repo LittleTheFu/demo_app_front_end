fefda1ad8f49fc518876e340e5d52f2218ffd72f
LittleTheFu <lttlstpd@live.com>
2021-09-05 21:13:10 +0800

    test slate

--- src/page/FriendPage.tsx
+++ src/page/FriendPage.tsx
@@ -1,9 +1,9 @@
-import React, { useMemo, useState } from 'react'
-import { createEditor, Descendant } from 'slate'
+import React, { useCallback, useMemo, useState } from 'react'
+import { createEditor, Descendant, Editor, Transforms } from 'slate'
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
 import { BaseEditor } from 'slate'
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomElement = { type: 'paragraph'|'code'; children: CustomText[] }
 type CustomText = { text: string }
 
 declare module 'slate' {
@@ -15,7 +15,6 @@ declare module 'slate' {
 }
 
 export const FriendPage: React.FC = () => {
-
   // const editor = createEditor();
   const editor = useMemo(() => withReact(createEditor()), [])
   // Add the initial value when setting up our state.
@@ -26,13 +25,55 @@ export const FriendPage: React.FC = () => {
     },
   ])
 
+  // Define a React component renderer for our code blocks.
+  const CodeElement = (props: {
+    attributes: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLPreElement> & React.HTMLAttributes<HTMLPreElement>;
+    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+  }) => {
+    return (
+      <pre {...props.attributes}>
+        <code>{props.children}</code>
+      </pre>
+    )
+  }
+
+  const DefaultElement = (props: {
+    attributes: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLParagraphElement> & React.HTMLAttributes<HTMLParagraphElement>;
+    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+  }) => {
+    return <p {...props.attributes}>{props.children}</p>
+  }
+
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
   return (
     <Slate
       editor={editor}
       value={value}
       onChange={newValue => setValue(newValue)}
     >
-      <Editable />
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={event => {
+          if (event.key === '`' && event.ctrlKey) {
+            // Prevent the "`" from being inserted by default.
+            event.preventDefault()
+            // Otherwise, set the currently selected blocks type to "code".
+            Transforms.setNodes(
+              editor,
+              { type: 'code' },
+              { match: n => Editor.isBlock(editor, n) }
+            )
+          }
+        }}
+      />
     </Slate>
   )
 }
