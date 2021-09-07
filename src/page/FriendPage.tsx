d3e9024d7ce0164635755a0ad7209db5583bdf4f
LittleTheFu <lttlstpd@live.com>
2021-09-07 21:17:15 +0800

    you can set bold attribute now

--- src/page/FriendPage.tsx
+++ src/page/FriendPage.tsx
@@ -1,10 +1,12 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { ReactElement, useCallback, useMemo, useState } from 'react'
 import { createEditor, Descendant, Editor, Transforms } from 'slate'
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
 import { BaseEditor } from 'slate'
+import { IconButton } from '@material-ui/core'
+import { FormatBold } from '@material-ui/icons'
 
-type CustomElement = { type: 'paragraph'|'code'; children: CustomText[] }
-type CustomText = { text: string }
+type CustomElement = { type: 'paragraph' | 'code'; children: CustomText[] }
+type CustomText = { text: string; bold: boolean }
 
 declare module 'slate' {
   interface CustomTypes {
@@ -16,12 +18,13 @@ declare module 'slate' {
 
 export const FriendPage: React.FC = () => {
   // const editor = createEditor();
-  const editor = useMemo(() => withReact(createEditor()), [])
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const renderLeaf = useCallback(props => <Leaf {...props} />, []);
   // Add the initial value when setting up our state.
   const [value, setValue] = useState<Descendant[]>([
     {
       type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
+      children: [{ text: 'A line of text in a paragraph.', bold: false }],
     },
   ])
 
@@ -53,14 +56,43 @@ export const FriendPage: React.FC = () => {
     }
   }, [])
 
+  const btnClick = (editor: Editor): void => {
+    const marks = Editor.marks(editor);
+    console.log('marks');
+    console.log(marks);
+    let mark = false;
+    if(marks && marks.bold) {
+      mark = marks.bold;
+    }
+    
+    editor.addMark('bold', !mark);
+    console.log('btn click');
+  }
+
+  type LeafProp = { attributes: any, children: ReactElement<any, any>, leaf: CustomText };
+  const Leaf = (props: LeafProp) => {
+    const { attributes, children, leaf } = props;
+    let new_children = children;
+    if (props.leaf.bold) {
+      new_children = <strong>{children}</strong>
+    }
+  
+    return <span {...attributes}>{new_children}</span>
+  }
+
   return (
     <Slate
       editor={editor}
       value={value}
       onChange={newValue => setValue(newValue)}
     >
+      <IconButton onClick={()=>{btnClick(editor)}}>
+        <FormatBold />
+      </IconButton>
+
       <Editable
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
         onKeyDown={event => {
           if (event.key === '`' && event.ctrlKey) {
             // Prevent the "`" from being inserted by default.
