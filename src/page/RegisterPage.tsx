2121114f1852d49fa8002b33bd326d1dac2ec480
LittleTheFu <lttlstpd@live.com>
2021-09-18 11:32:37 +0800

    clear code

--- src/page/RegisterPage.tsx
+++ src/page/RegisterPage.tsx
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button';
 import { ISimpleData, postRegister } from '../common/service';
 import { useHistory, Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
-import { RetMsgObj } from '../common/service';
 import { validate } from 'email-validator';
 import {
     isValidPassowrd,
