40490eb42030e1798fe0d91551ac3ad359101978
LittleTheFu <lttlstpd@live.com>
2021-09-18 16:27:43 +0800

    redirect to login page if you changed your password successfully

--- src/page/ResetPasswordPage.tsx
+++ src/page/ResetPasswordPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@material-ui/core";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { resetPassword } from "../common/service";
@@ -6,22 +7,34 @@ import { AccountInputCard } from "../component/AccountInputCard";
 export const ResetPasswordPage: React.FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [done, setDone] = useState(false);
 
     const { code } = useParams<{ code: string }>();
 
     const applyClick = () => {
         resetPassword(email, password, code, (data) => {
+            setDone(true);
             console.log(data);
         });
     }
 
     return (
-        <AccountInputCard
-            code={code}
-            email={email}
-            password={password}
-            onEmailChange={setEmail}
-            onPasswordChange={setPassword}
-            applyClick={applyClick} />
+        done
+            ?
+            <div>
+                <Link href='http://localhost:3000/login'>
+                    <h3>
+                        done! go to login page
+                    </h3>
+                </Link>
+            </div>
+            :
+            <AccountInputCard
+                code={code}
+                email={email}
+                password={password}
+                onEmailChange={setEmail}
+                onPasswordChange={setPassword}
+                applyClick={applyClick} />
     )
 };
