9f2d54319325d60fde3a5fea175d154f94a6eeaf
LittleTheFu <lttlstpd@live.com>
2021-09-24 22:32:44 +0800

    fix rich editor's size

--- src/component/RichEditor.tsx
+++ src/component/RichEditor.tsx
@@ -44,10 +44,10 @@ const useStyles = makeStyles(() =>
         },
         editor: {
             // height: 300,
-            // width: 800,
-            // height: 800,
+            width: 800,
+            height: 800,
             border: '1px solid #000',
-            // overflow: 'auto',
+            overflow: 'auto',
         },
     }),
 );
