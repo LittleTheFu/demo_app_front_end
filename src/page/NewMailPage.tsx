7b9b5cc728c7bc98af514a338e5087620fa249d0
fuyuxiang <lttlstpd@live.com>
2022-07-19 09:56:52 +0800

    fix layout of mail page

--- src/page/NewMailPage.tsx
+++ src/page/NewMailPage.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@material-ui/core";
+import { Box, Button, makeStyles, TextField } from "@material-ui/core";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { UserHead } from "../component/UserHead";
@@ -10,10 +10,25 @@ interface stateType {
   name: string;
 }
 
+const useStyles = makeStyles({
+  typeBox: {
+    width: "80%",
+    margin: "auto",
+  },
+  typeText: {
+    width: "100%",
+  },
+  replyButton: {
+    width: "100%",
+  },
+});
+
 export const NewMailPage: React.FC = () => {
   const [content, setContent] = useState("");
   const location = useLocation<stateType>();
   const { state } = location;
+
+  const classes = useStyles();
 
   const SendClick = (): void => {
     createMail(state.id, content, (data) => {
@@ -24,23 +39,27 @@ export const NewMailPage: React.FC = () => {
   return (
     <div>
       <UserHead userName={state.name} avatar={state.icon} />
-      <TextField
-        id="outlined-multiline-static"
-        multiline
-        rows={4}
-        variant="outlined"
-        onChange={(e): void => {
-          setContent(e.target.value);
-        }}
-      />
-      <Button
-        onClick={SendClick}
-        type="submit"
-        variant="contained"
-        color="primary"
-      >
-        reply
-      </Button>
+      <Box className={classes.typeBox}>
+        <TextField
+          className={classes.typeText}
+          id="outlined-multiline-static"
+          multiline
+          rows={4}
+          variant="outlined"
+          onChange={(e): void => {
+            setContent(e.target.value);
+          }}
+        />
+        <Button
+          className={classes.replyButton}
+          onClick={SendClick}
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          reply
+        </Button>
+      </Box>
     </div>
   );
 };
